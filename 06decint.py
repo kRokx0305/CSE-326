208a4eb9b2f6b5450e828af6308d071d63560dc1
Karan More <117158677+kRokx0305@users.noreply.github.com>
2022-12-11 23:33:16 +0530

    Add files via upload

---
+++ 06decint.py
@@ -0,0 +1,14 @@
+xyz=("a","b","c","d","e","f")
+
+#mebership
+print('a'in xyz)
+print('e'in xyz)
+print('z'in xyz)
+
+#not in operation
+print('a'not in xyz)
+print('z'not in xyz)
+
+name=("abc","xyz","pqr")
+for i in name:
+    print("hello",i)
